52e99ea28444da2b138390937617527b0a9c00f1
hsonne <hauke.sonnenberg@gmx.de>
2019-05-25 19:20:24 +0200

    Add "lean" versions of summary functions
    These funcions do not require the "Name" columns but
    only the corresponding "ID" columns. The merge of the
    "Name" columns is done after summarising. This allows
    for smaller input data frames and much faster
    processing.
    
    Add "reverse_engineer.R", a script to test whether
    the exact same results as with the original
    functions can be achieved with the "lean" functions,
    called on the "lean" data frames of the risk object.

--- R/get_risk_logremoval_stats.R
+++ R/get_risk_logremoval_stats.R
@@ -20,3 +20,32 @@ get_risk_logremoval_stats <- function(data)
       max = max(.data$logreduction)
     )
 }
+
+# get_risk_logremoval_stats_lean -----------------------------------------------
+
+#' "Lean" version of get_risk_logremoval_stats()
+#' 
+get_risk_logremoval_stats_lean <- function(data, config)
+{
+  data %>% 
+    dplyr::group_by(
+      .data$TreatmentSchemeID, 
+      .data$TreatmentID, 
+      .data$PathogenGroup
+    ) %>% 
+    dplyr::summarise( 
+      min = min(.data$logreduction), 
+      p05 = quantile(.data$logreduction, probs = 0.05),
+      p25 =  quantile(.data$logreduction, probs = 0.25),
+      mean = mean(.data$logreduction), 
+      median = median(.data$logreduction), 
+      p75 =  quantile(.data$logreduction, probs = 0.75),
+      p95 = quantile(.data$logreduction, probs = 0.95),
+      max = max(.data$logreduction)
+    ) %>% 
+    kwb.qmra:::add_scheme_name(config) %>%
+    kwb.qmra:::add_treatment_name(config) %>%
+    kwb.utils::moveColumnsToFront(c(
+      scheme_columns(), treatment_columns(), "PathogenGroup"
+    ))
+}
--- R/get_risk_total_stats.R
+++ R/get_risk_total_stats.R
@@ -32,3 +32,44 @@ get_risk_total_stats <- function(data)
       max = max(.data$value)
     )
 }
+
+# get_risk_total_stats_lean ----------------------------------------------------
+
+#' Lean version of get_risk_total_stats()
+#' 
+#' This function returns the same as get_risk_total_stats() but does not require
+#' the "Name" columns. Instead, names are merged by ID from the metadata tables
+#' given in "config".
+#' 
+get_risk_total_stats_lean <- function(data_lean, config)
+{
+  tidyr::gather(
+    data = data_lean, 
+    key = "key", 
+    value = "value",  
+    - .data$repeatID, 
+    - .data$TreatmentSchemeID, 
+    - .data$PathogenID
+  ) %>% 
+    dplyr::group_by(
+      .data$TreatmentSchemeID, 
+      .data$PathogenID,
+      .data$key
+    ) %>% 
+    dplyr::summarise( 
+      min = min(.data$value), 
+      p05 = quantile(.data$value, probs = 0.05),
+      p25 =  quantile(.data$value, probs = 0.25),
+      mean = mean(.data$value), 
+      median = median(.data$value), 
+      p75 =  quantile(.data$value, probs = 0.75),
+      p95 = quantile(.data$value, probs = 0.95),
+      max = max(.data$value)
+    ) %>%
+    add_scheme_name(config) %>%
+    add_pathogen_name(config) %>%
+    dplyr::arrange(.data$TreatmentSchemeID, .data$PathogenGroup) %>%
+    kwb.utils::moveColumnsToFront(c(
+      scheme_columns(), pathogen_columns(), "key"
+    ))
+}
