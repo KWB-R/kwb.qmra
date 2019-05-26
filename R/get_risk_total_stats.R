963d8054a413f67c4de7618f3f88145f8b387c83
hsonne <hauke.sonnenberg@gmx.de>
2019-05-26 19:09:34 +0200

    Clean get_risk_total_stats.R
    Avoid duplication and improve readability by means of
    new functions gather_total_risk(),
    group_by_treatment_and_pathogen(), summarise_value()

--- R/get_risk_total_stats.R
+++ R/get_risk_total_stats.R
@@ -1,36 +1,77 @@
 # get_risk_total_stats ---------------------------------------------------------
 get_risk_total_stats <- function(data)
 {
-  tidyr::gather(
-    data = data, 
-    key = "key", 
-    value = "value",  
-    - .data$TreatmentSchemeID, 
-    - .data$TreatmentSchemeName, 
-    - .data$PathogenGroup, 
-    - .data$repeatID, 
-    .data$events, 
-    - .data$PathogenID, 
-    - .data$PathogenName
-  ) %>% 
+  data %>% 
+    gather_total_risk(lean = FALSE) %>% 
+    group_by_treatment_and_pathogen(lean = FALSE) %>% 
+    summarise_value()
+}
+
+# gather_total_risk ------------------------------------------------------------
+gather_total_risk <- function(data, lean)
+{
+  if (lean) {
+    tidyr::gather(
+      data, 
+      key = "key", 
+      value = "value",  
+      - .data$repeatID, 
+      - .data$TreatmentSchemeID, 
+      - .data$PathogenID
+    )
+  } else {
+    tidyr::gather(
+      data = data, 
+      key = "key", 
+      value = "value",  
+      - .data$TreatmentSchemeID, 
+      - .data$TreatmentSchemeName, 
+      - .data$PathogenGroup, 
+      - .data$repeatID, 
+      .data$events, 
+      - .data$PathogenID, 
+      - .data$PathogenName
+    )
+  }
+}
+
+# group_by_treatment_and_pathogen ----------------------------------------------
+group_by_treatment_and_pathogen <- function(data, lean)
+{
+  if (lean) {
     dplyr::group_by(
+      data,
+      .data$TreatmentSchemeID, 
+      .data$PathogenID,
+      .data$key
+    )     
+  } else {
+    dplyr::group_by(
+      data,
       .data$TreatmentSchemeID, 
       .data$TreatmentSchemeName, 
       .data$PathogenGroup, 
       .data$PathogenID, 
       .data$PathogenName, 
-      key
-    ) %>% 
-    dplyr::summarise( 
-      min = min(.data$value), 
-      p05 = quantile(.data$value, probs = 0.05),
-      p25 =  quantile(.data$value, probs = 0.25),
-      mean = mean(.data$value), 
-      median = median(.data$value), 
-      p75 =  quantile(.data$value, probs = 0.75),
-      p95 = quantile(.data$value, probs = 0.95),
-      max = max(.data$value)
+      .data$key
     )
+  }
+}
+
+# summarise_value --------------------------------------------------------------
+summarise_value <- function(data)
+{
+  dplyr::summarise(
+    data,
+    min = min(.data$value), 
+    p05 = quantile(.data$value, probs = 0.05),
+    p25 =  quantile(.data$value, probs = 0.25),
+    mean = mean(.data$value), 
+    median = median(.data$value), 
+    p75 =  quantile(.data$value, probs = 0.75),
+    p95 = quantile(.data$value, probs = 0.95),
+    max = max(.data$value)
+  )
 }
 
 # get_risk_total_stats_lean ----------------------------------------------------
@@ -43,29 +84,10 @@ get_risk_total_stats <- function(data)
 #' 
 get_risk_total_stats_lean <- function(data_lean, config)
 {
-  tidyr::gather(
-    data = data_lean, 
-    key = "key", 
-    value = "value",  
-    - .data$repeatID, 
-    - .data$TreatmentSchemeID, 
-    - .data$PathogenID
-  ) %>% 
-    dplyr::group_by(
-      .data$TreatmentSchemeID, 
-      .data$PathogenID,
-      .data$key
-    ) %>% 
-    dplyr::summarise( 
-      min = min(.data$value), 
-      p05 = quantile(.data$value, probs = 0.05),
-      p25 =  quantile(.data$value, probs = 0.25),
-      mean = mean(.data$value), 
-      median = median(.data$value), 
-      p75 =  quantile(.data$value, probs = 0.75),
-      p95 = quantile(.data$value, probs = 0.95),
-      max = max(.data$value)
-    ) %>%
+  data_lean %>% 
+    gather_total_risk(lean = TRUE) %>%
+    group_by_treatment_and_pathogen(lean = TRUE) %>% 
+    summarise_value() %>%
     add_scheme_name(config) %>%
     add_pathogen_name(config) %>%
     dplyr::arrange(.data$TreatmentSchemeID, .data$PathogenGroup) %>%
