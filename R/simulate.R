9788c19627df78205980ed6cf319963c95efcb3e
hsonne <hauke.sonnenberg@gmx.de>
2019-05-26 19:45:35 +0200

    Move functions from simulate.R to utils.R
    - number_of_exposures()
    - number_of_repeatings()
    - get_exposure_value_or_stop()

--- R/simulate.R
+++ R/simulate.R
@@ -1,38 +1,3 @@
-# number_of_exposures ----------------------------------------------------------
-
-#' Helper function: gets number of exposures from config
-#' @param config as retrieved by config_read() 
-#' @return number of exposures
-#' @keywords internal
-number_of_exposures <- function(config)
-{
-  # HARD CODED: number_of_exposures MUST be of type value!      
-  get_exposure_value_or_stop(config, "number_of_exposures")
-}
-
-# number_of_repeatings ---------------------------------------------------------
-
-#' Helper function: gets number of repeatings from config
-#' @param config as retrieved by config_read() 
-#' @return number of repeatings (used for bayesian analysis)
-#' @keywords internal
-number_of_repeatings <- function(config)
-{
-  # HARD CODED: number_of_repeatings MUST be of type value!      
-  get_exposure_value_or_stop(config, "number_of_repeatings")
-}
-
-# get_exposure_value_or_stop ---------------------------------------------------
-get_exposure_value_or_stop <- function(config, name)
-{
-  ### HARD CODED: "name" MUST be of type value!    
-  if (is.na(value <- config$exposure$value[config$exposure$name == name])) {
-    stop(sprintf(get_stop_text("must_be_value"), name), call. = FALSE)
-  }
-  
-  value
-}
-
 # simulate_inflow --------------------------------------------------------------
 
 #' Simulate: inflow
--- R/utils.R
+++ R/utils.R
@@ -19,6 +19,19 @@ add_treatment_name <- function(data, config)
   dplyr::inner_join(data, treatment_names, by = "TreatmentID")
 }
 
+# get_exposure_value_or_stop ---------------------------------------------------
+get_exposure_value_or_stop <- function(config, name)
+{
+  # HARD CODED: "name" MUST be of type value!
+  value <- config$exposure$value[config$exposure$name == name]
+
+  if (is.na(value)) {
+    stop(sprintf(get_stop_text("must_be_value"), name), call. = FALSE)
+  }
+  
+  value
+}
+
 # id_columns_to_integer --------------------------------------------------------
 id_columns_to_integer <- function(data)
 {
@@ -28,6 +41,30 @@ id_columns_to_integer <- function(data)
   data[to_convert] <- lapply(data[to_convert], as.integer)
   
   data
+}
+
+# number_of_exposures ----------------------------------------------------------
+
+#' Helper function: gets number of exposures from config
+#' @param config as retrieved by config_read() 
+#' @return number of exposures
+#' @keywords internal
+number_of_exposures <- function(config)
+{
+  # HARD CODED: number_of_exposures MUST be of type value!      
+  get_exposure_value_or_stop(config, "number_of_exposures")
+}
+
+# number_of_repeatings ---------------------------------------------------------
+
+#' Helper function: gets number of repeatings from config
+#' @param config as retrieved by config_read() 
+#' @return number of repeatings (used for bayesian analysis)
+#' @keywords internal
+number_of_repeatings <- function(config)
+{
+  # HARD CODED: number_of_repeatings MUST be of type value!      
+  get_exposure_value_or_stop(config, "number_of_repeatings")
 }
 
 # pathogen_columns -------------------------------------------------------------
@@ -47,3 +84,4 @@ treatment_columns <- function()
 {
   c("TreatmentID", "TreatmentName")
 }
+
