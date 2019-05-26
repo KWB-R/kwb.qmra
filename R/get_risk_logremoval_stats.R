f070c24651d612cb65bfc6954b407ab76b2fad81
hsonne <hauke.sonnenberg@gmx.de>
2019-05-26 18:49:09 +0200

    Clean get_risk_logremoval_stats.R
    Avoid duplication and improve readability by means of
    new functions group_by_treatment_and_pathogen_group(),
    summarise_logreduction()

--- R/get_risk_logremoval_stats.R
+++ R/get_risk_logremoval_stats.R
@@ -2,23 +2,46 @@
 get_risk_logremoval_stats <- function(data)
 {
   data %>% 
+    group_by_treatment_and_pathogen_group(lean = FALSE) %>% 
+    summarise_logreduction()
+}
+
+# group_by_treatment_and_pathogen_group ----------------------------------------
+group_by_treatment_and_pathogen_group <- function(data, lean)
+{
+  if (lean) {
     dplyr::group_by(
+      data,
+      .data$TreatmentSchemeID, 
+      .data$TreatmentID, 
+      .data$PathogenGroup
+    )
+  } else {
+    dplyr::group_by(
+      data,
       .data$TreatmentSchemeID, 
       .data$TreatmentSchemeName, 
       .data$TreatmentID, 
       .data$TreatmentName, 
       .data$PathogenGroup
-    ) %>% 
-    dplyr::summarise( 
-      min = min(.data$logreduction), 
-      p05 = quantile(.data$logreduction, probs = 0.05),
-      p25 =  quantile(.data$logreduction, probs = 0.25),
-      mean = mean(.data$logreduction), 
-      median = median(.data$logreduction), 
-      p75 =  quantile(.data$logreduction, probs = 0.75),
-      p95 = quantile(.data$logreduction, probs = 0.95),
-      max = max(.data$logreduction)
     )
+  }
+}
+
+# summarise_logreduction -------------------------------------------------------
+summarise_logreduction <- function(data)
+{
+  dplyr::summarise( 
+    data,
+    min = min(.data$logreduction), 
+    p05 = quantile(.data$logreduction, probs = 0.05),
+    p25 =  quantile(.data$logreduction, probs = 0.25),
+    mean = mean(.data$logreduction), 
+    median = median(.data$logreduction), 
+    p75 =  quantile(.data$logreduction, probs = 0.75),
+    p95 = quantile(.data$logreduction, probs = 0.95),
+    max = max(.data$logreduction)
+  )
 }
 
 # get_risk_logremoval_stats_lean -----------------------------------------------
@@ -28,21 +51,8 @@ get_risk_logremoval_stats <- function(data)
 get_risk_logremoval_stats_lean <- function(data, config)
 {
   data %>% 
-    dplyr::group_by(
-      .data$TreatmentSchemeID, 
-      .data$TreatmentID, 
-      .data$PathogenGroup
-    ) %>% 
-    dplyr::summarise( 
-      min = min(.data$logreduction), 
-      p05 = quantile(.data$logreduction, probs = 0.05),
-      p25 =  quantile(.data$logreduction, probs = 0.25),
-      mean = mean(.data$logreduction), 
-      median = median(.data$logreduction), 
-      p75 =  quantile(.data$logreduction, probs = 0.75),
-      p95 = quantile(.data$logreduction, probs = 0.95),
-      max = max(.data$logreduction)
-    ) %>% 
+    group_by_treatment_and_pathogen_group(lean = TRUE) %>% 
+    summarise_logreduction() %>% 
     kwb.qmra:::add_scheme_name(config) %>%
     kwb.qmra:::add_treatment_name(config) %>%
     kwb.utils::moveColumnsToFront(c(
