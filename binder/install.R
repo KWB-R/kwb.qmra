bb1c6e4cf75ba5485c20f1a171b8890847a0be39
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2020-08-20 21:09:05 +0200

    try fixing CRAN versions for pkg dependencies

--- binder/install.R
+++ binder/install.R
@@ -1,2 +1,9 @@
-install.packages("devtools")
-devtools::install_github("kwb-r/kwb.qmra@dev")
+cran_pkgs <- c("assertthat", "cli", "colorspace", "crayon", "digest", 
+"dplyr", "ellipsis", "EnvStats", "fansi", "ggplot2", "glue", "gtable", 
+"hms", "htmltools", "httpuv", "later", "lazyeval", "lifecycle", "magrittr", 
+"mime", "munsell", "pillar", "pkgconfig", "plyr", "promises", "purrr", 
+"R6", "Rcpp", "readr", "remotes", "rlang", "rstudioapi", "scales", "sessioninfo", 
+"sfsmisc", "shiny", "tibble", "tidyr", "tidyselect", "utf8", "vctrs", 
+"withr", "xtable")
+install.packages(cran_pkgs)
+remotes::install_github("kwb-r/kwb.qmra@dev")
